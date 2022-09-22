d1430dbfb488c4ee79decc0f1b7c8cce34cc5739
kevinloeffler <hi@kevinloeffler.ch>
2022-09-22 17:55:49 +0200

    Initial commit

---
+++ server/database-manager.js
@@ -0,0 +1,44 @@
+import dotenv from 'dotenv'
+import * as pg from 'pg'
+
+dotenv.config()
+
+const { Pool } = pg.default
+const pool = new Pool ({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+})
+
+pool.connect()
+
+/* QUERIES */
+
+async function getQueue(limit = 33) {
+    const query = "SELECT * FROM songs WHERE played = false AND deleted = false LIMIT $1 "
+    const values = [limit]
+
+    try {
+        const res = await pool.query(query, values)
+        return res.rows
+    } catch (err) {
+        console.log('DB ERROR:', err)
+    }
+}
+
+async function addSong(id, title, artist, singer, visible = true) {
+    const query = 'INSERT INTO songs (request_id, song_name, artist_name, singer_name, visible, played, deleted)' +
+        'VALUES ($1, $2, $3, $4, $5, false, false)'
+    const values = [id, title, artist, singer, visible]
+
+    try {
+        const res = await pool.query(query, values)
+        return res.rows
+    } catch (err) {
+        console.log('DB ERROR:', err)
+    }
+}
+
+
+
+
+export {getQueue, addSong}
