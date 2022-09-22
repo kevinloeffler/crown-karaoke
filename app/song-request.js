4cb046b611d8a0878a65093200dd75a7ba7047b2
kevinloeffler <hi@kevinloeffler.ch>
2022-09-22 15:38:48 +0200

    ADD: connectivity, data model and render methods

--- app/song-request.js
+++ app/song-request.js
@@ -3,5 +3,91 @@
 const titleInput = document.querySelector('#title-input')
 const artistInput = document.querySelector('#artist-input')
 const singerInput = document.querySelector('#singer-input')
+
+const overlay = document.querySelector('#overlay')
+const overlayTitle = document.querySelector('#overlay-title')
+const overlayArtist = document.querySelector('#overlay-artist')
+
 const confirmSongButton = document.querySelector('#confirm-song-button')
+confirmSongButton.addEventListener('click', handleButtonClick)
+
+function renderOverlay(status) {
+    if (status) {
+        overlayTitle.textContent = songRequest.title
+        overlayArtist.textContent = songRequest.artist
+    } else {
+        // ToDo: Handle error status
+        console.log('Error: song was not added to wait list')
+    }
+}
+
+/* MODEL */
+
+const songRequest = {
+    id: null,
+    title: '',
+    artist: '',
+    singer: '',
+}
+
+const handleMessage = {
+    'hello': handleHelloMsg,
+    'song-confirmation': handleSongConfirmationMsg,
+}
+
+function handleHelloMsg(msg) {
+    console.log(`Hello message received at ${msg.data}`)
+}
+
+function handleSongConfirmationMsg(msg) {
+    console.log(`Song request confirmed, id: ${msg.id}`)
+    if (msg.id === songRequest.id) {
+        renderOverlay(msg.status)
+    }
+}
+
+/* LOGIC */
+
+function handleButtonClick() {
+    if (!(titleInput.value === '' || artistInput.value === '')) {
+        const requestID = Math.floor(Math.random() * 100_000)
+
+        songRequest.id = requestID
+        songRequest.title = titleInput.value
+        songRequest.artist = artistInput.value
+        songRequest.singer = singerInput.value
+
+        const request = {
+            type: 'request-song',
+            id: requestID,
+            title: songRequest.title,
+            artist: songRequest.artist,
+            singer: songRequest.singer,
+        }
+        sendSongRequest(request)
+    } else {
+        alert('Titel und Artist müssen ausgefüllt sein!')
+    }
+}
+
+/* CONNECTION */
+
+const wsConnection = new WebSocket('ws://localhost:3000/echo', )
+wsConnection.addEventListener('message', handleWebsocketMessages)
+
+function sendSongRequest(request) {
+    wsConnection.send(JSON.stringify(request))
+}
+
+function handleWebsocketMessages(rawMsg) {
+    try {
+        const msg = JSON.parse(rawMsg.data)
+        console.log('received:', msg)
+
+        handleMessage[msg.type](msg)
+
+    } catch (err) {
+        console.log(err)
+    }
+}
 
