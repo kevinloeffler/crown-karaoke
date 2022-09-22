597ce77da7501ffea837f384c33ed67f2b0beb7f
kevinloeffler <hi@kevinloeffler.ch>
2022-09-22 09:35:12 +0200

    ADD: build song request UI

---
+++ app/song-request.js
@@ -0,0 +1,7 @@
+/* VIEW */
+
+const titleInput = document.querySelector('#title-input')
+const artistInput = document.querySelector('#artist-input')
+const singerInput = document.querySelector('#singer-input')
+const confirmSongButton = document.querySelector('#confirm-song-button')
+
