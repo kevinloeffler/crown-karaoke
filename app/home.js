b5bbb6bca533ee468429ea32f0dc0fe8932c310a
Kevin Loeffler <hi@kevinloeffler.ch>
2022-09-23 11:23:48 +0200

    ADD: renderSongs and renderWaitTime methods REFACTOR: wsConnection message handling

--- app/home.js
+++ app/home.js
@@ -1,12 +1,9 @@
 let WS_URL
-console.log('cur:', window.location.href)
 if (window.location.href === 'http://localhost:3000/') {
     WS_URL = 'ws://localhost:3000/echo'
 } else {
     WS_URL = 'ws://crown-karaoke.herokuapp.com/echo'
 }
-
-console.log('WS URL:', WS_URL)
 
 /* VIEW */
 
@@ -19,19 +16,36 @@ const nowPlayingSinger = document.querySelector('#now-playing-singer')
 const queueWrapper = document.querySelector('#queue-wrapper')
 const waitTimeIndicator = document.querySelector('#wait-time')
 
+function renderSongs(songs) {
+    let fragment = ''
+
+    for (const song of songs) {
+        console.log(song)
+        fragment += `<div class="song-wrapper"><p>${song.song_name}</p><p>${song.artist_name}</p></div>`
+    }
+    console.log(fragment)
+    queueWrapper.innerHTML = fragment
+}
+
+function renderWaitTime(songs_count) {
+    waitTimeIndicator.textContent = `~ ${songs_count * 3} Min Wartezeit`
+}
+
 /* MODEL */
 
 const handleMessage = {
     'hello': handleHelloMsg,
-    'broadcast': handleBroadcastMsg,
+    'update': handleUpdateMsg,
 }
 
 function handleHelloMsg(msg) {
     console.log(`Hello message received at ${msg.data}`)
 }
 
-function handleBroadastMsg(msg) {
-    
+function handleUpdateMsg(msg) {
+    console.log('Update received')
+    renderSongs(msg.data)
+    renderWaitTime(msg.data.length)
 }
 
 /* CONNECTION */
@@ -42,7 +56,21 @@ wsConnection.onopen = function(rawMsg) {
     console.log('ws connection opened')
 }
 
-wsConnection.onmessage = function(rawMsg) {
+wsConnection.onmessage = async (rawMsg) => {
+    try {
+        const msg = JSON.parse(rawMsg.data)
+        await handleMessage[msg.type](msg, wsConnection)
+
+    } catch (err) {
+        if (err instanceof SyntaxError) {
+            console.log('received message is not valid JSON')
+        } else {
+            console.log(err)
+        }
+    }
+
+
+/*
     try {
         const msg = JSON.parse(rawMsg.data)
         console.log('received:', msg)
@@ -63,7 +91,7 @@ wsConnection.onmessage = function(rawMsg) {
         console.log('error:')
         console.log(err.name)
     }
-
+*/
     // const time = new Date(msg.date)
     // const timeStr = time.toLocaleTimeString()
 }
